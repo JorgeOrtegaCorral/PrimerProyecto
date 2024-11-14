d1124231ddef9dc4a923f99bdc6afd3c0946c361
JorgeOrtegaCorral <jorge.corral@estudiante.epsum.school>
2024-11-14 13:32:25 +0100

    interfaz para detalles del pokemon y ahora mostrando las habilidades con una interfaz para estas(interfaz ability dentro de interfaz detalles)

--- src/app/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonDetailService } from "../services/pokemon/pokemon-detail.service";
 import {DetailService} from '../services/detalles-pokemon/detail.service';
+import {Detalles} from '../services/interfaces/detalles';
 
 @Component({
   selector: 'app-pokemon-detail',
@@ -9,7 +10,7 @@ import {DetailService} from '../services/detalles-pokemon/detail.service';
 })
 export class PokemonDetailComponent implements OnInit{
   nombre: string = "";
-  sprite: string = "";
+  pokemon: Detalles | null= null;
 
   constructor(
     private pokemonDetailService: PokemonDetailService,
@@ -22,13 +23,18 @@ export class PokemonDetailComponent implements OnInit{
 
      this.detailService.getPokemon(this.nombre).subscribe({
 
-       //Obligatorios: next, error
-       //Opcional: complete
-
        next: data => { //Si la comunicacion y la respuesta esta ok
          //console.log(data)
-         this.sprite = data.sprites.front_default
-         console.log(this.sprite)
+         this.pokemon = {
+           imagen: data.sprites.front_default,
+           abilities: []
+         }
+
+         for (let i = 0; i < data.abilities.length; i++){
+           this.pokemon.abilities.push(data.abilities[i].ability);
+         }
+
+         console.log(this.pokemon)
        },
 
        //Si hay error a la hora de comunicarnos con con la api o problemas
---
+++ src/app/services/interfaces/detalles.ts
@@ -0,0 +1,9 @@
+export interface Ability{
+  name: string;
+  url: string;
+}
+
+export interface Detalles {
+  imagen: string;
+  abilities: Ability[];
+}
